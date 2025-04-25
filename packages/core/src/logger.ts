8cd3e57047d0c9c7e47cb42df28baee4c6765d88
Sergio Salcedo <53319615+vssalcedo@users.noreply.github.com>
2025-04-25 15:10:49 +0200

    Trigger linter

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -4,7 +4,9 @@
  */
 
 export interface Logger {
-  info(message: string, context?: any): void;
+
+
+info(message: string, context?: any): void;
   warn(message: string, context?: any): void;
   error(message: string, context?: any): void;
   debug?(message: string, context?: any): void;
